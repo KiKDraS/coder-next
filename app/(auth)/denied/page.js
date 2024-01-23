960b85d188b37a378d37bf280bbd57a29425eead
Anahí Foresi <anahi.foresi@trully.ai>
2024-01-22 21:59:55 -0300

    páginas 404 y 403

--- app/(auth)/denied/page.js
+++ app/(auth)/denied/page.js
@@ -1,4 +1,18 @@
+import ErrorPage from "@/app/components/ErrorPage";
+import { APP_IMAGES } from "@/app/constants";
+
 const Denied = () => {
-  return <div>Denied</div>;
+  return (
+    <ErrorPage
+      imageConfig={{
+        img_url: APP_IMAGES.PAGE_403,
+        alt: "Página 403",
+        width: 500,
+        height: 500,
+      }}
+      title="Hubo un problema"
+      text="No pudimos encontrar la página que estás buscando."
+    />
+  );
 };
 export default Denied;
