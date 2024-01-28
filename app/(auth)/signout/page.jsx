c41128677b706351c3c9093722296c063a6ff3c5
Anahí Foresi <anahi.foresi@trully.ai>
2024-01-27 23:19:00 -0300

    ajuste del bg light

--- app/(auth)/signout/page.jsx
+++ app/(auth)/signout/page.jsx
@@ -3,7 +3,7 @@ import SignOutButtons from "@/app/components/client-side/SignOutButtons";
 
 const SignOut = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <section className="flex items-center justify-center min-h-screen bg-slate-200 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center gap-y-4 w-2/4 min-w-[350px] max-w-lg p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
         <Logo width={250} height={91.7} classes="pb-2" />
         <p className="mb-4 text-gray-500 dark:text-gray-300">
@@ -11,7 +11,7 @@ const SignOut = () => {
         </p>
         <SignOutButtons />
       </div>
-    </div>
+    </section>
   );
 };
 
